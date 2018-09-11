fcaf198d3dd338dedaec62bdb3d9904b7d5a6a43
ppolanowsk <polanowski20@wp.pl>
2018-09-11 12:21:08 +0200

    dodanie twojej starej]

---
+++ python/szablon.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  szablon.py
+#  
+def main(args):
+    print("Eluwa psy co jest")
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
