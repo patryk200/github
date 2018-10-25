d478679cfb8676b3c634118357b2a92434071169
patryk200 <polanowski20@wp.pl>
2018-10-25 07:56:27 +0200

    Euklides optymalny

--- cpp/euklides/main.cpp
+++ cpp/euklides/main.cpp
@@ -1,20 +1,36 @@
 #include <iostream>
 
 using namespace std;
-int nwd klasyczne(int a, int b){
+int nwd_klasyczne(int a, int b){
+    int licznik = 0
     while (a != b){
         if(a > b)
             a = a - b;
         else
         b = b - a;
+        licznik++;
     }
+    cout << "Powtórzeń: " << licznik << endl;
     return a;
 }
 
+int nwd_optymalny(int a, int b) {
+    int licznik = 0;
+    while (a > 0){
+        a = a % b;
+        b = b - a;
+        licznik++;
+    }
+    cout << "Powtórzeń: " << licznik << endl;
+    return b;
+}
 int main()
 {
     int a, b;
     a = b = 0;
-    cout << "Podaj dwie liczby:"
+    cout << "Podaj dwie liczby:";
+    cin >> a >> b;
+    cout << "nwd(" << a << " , " << b << ") = "
+        << nwd_klasyczne(a, b) << endl;
     return 0;
 }
